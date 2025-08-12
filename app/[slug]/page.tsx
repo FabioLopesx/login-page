dbacc0b03ebc052c539501632f3f2a459d496631
Fábio Lopes <fabiolopesx6@gmail.com>
2025-08-12 10:00:55 -0300

    feat: crianção pagina pos login

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,11 @@
+const paginaCliente = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1>Página do Cliente</h1>
+      <p>Bem-vindo à sua página personalizada!</p>
+      <p>Nome:</p>
+    </div>
+  );
+};
+
+export default paginaCliente;
